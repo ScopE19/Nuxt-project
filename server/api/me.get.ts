83ff03b2aed385972310c57effb2455e6c9f4bb7
ScopE19 <danatello2003@gmail.com>
2025-04-22 14:39:00 +0500

    dassssss

--- server/api/me.get.ts
+++ server/api/me.get.ts
@@ -1,4 +1,4 @@
-import { getServerSession } from "next-auth";
+import { getServerSession } from "#auth";
 export default eventHandler(async (event) => {
     const session = await getServerSession(event)
     console.log(session)
