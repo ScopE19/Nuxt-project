3f233cf6d026561a1cecbeb19aeee2b540ce4882
ScopE19 <danatello2003@gmail.com>
2025-04-28 10:20:06 +0500

    dsaaaaaaaa

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,13 @@
 <template>
-    <main>
-        <p>JOPA</p>
+    <main class="w-screen h-screen flex flex-col items-center justify-center from-purple-900 bg-gradient-to-br to-red-900 m-[-2px]">
+        <p class="text-2xl textshadow">Welcome to my website</p>
     </main>
 </template>
+<script setup>
+definePageMeta({
+  auth: {
+    unauthenticatedOnly: true,
+    globalMiddleware: true,
+  },
+})
+</script>
